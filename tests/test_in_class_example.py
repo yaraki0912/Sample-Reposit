a57b17ed83b7178e1eeaca64abb87ff235d47057
yaraki <yaraki@u.rochester.edu>
2018-09-17 20:35:57 -0400

    test greeting function

--- tests/test_in_class_example.py
+++ tests/test_in_class_example.py
@@ -1,2 +1,14 @@
-import unittest
-import numpy
+import pytest
+
+from click.testing import CliRunner
+
+from example import *
+
+class TestExample(unittest.TestCase):
+#@pytest.fixture
+        
+        
+
+    def TestGreetingStudent(self):
+        assert GreetingStudent("2019")
+        self.assertEqual("Welcome back seniors",GreetingStudent)
