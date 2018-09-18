5532d5784226ce673df447cb03399cd440b8695c
yaraki <yaraki@u.rochester.edu>
2018-09-17 20:21:53 -0400

    greeting function

---
+++ example/example.py
@@ -0,0 +1,12 @@
+import unittest
+
+def GreetingStudent(year):
+    if year == "2019"
+       return "Welcome back seniors"
+    if year == "2020"
+        return "Welcome back juniors" 
+    if year == "2021"
+        return "Welcome back sophmores"
+    if year == "2022"
+        return "Welcome to college"  
+        
